3b5bed8710e144c00d3643ae658bdc675532e131
adaluong <35322993+adaluong@users.noreply.github.com>
2022-09-13 15:28:09 +1000

    Add tut01 code

---
+++ tut01/cat.c
@@ -0,0 +1,14 @@
+// Basic Hello World program
+// Marc Chee, September 2020
+
+#include <stdio.h>
+
+int main (void) {
+
+    // printf("Hello World\n");
+    printf("/\\___/\\\n");
+    printf("(=' w '=)\n");
+
+    return 0;
+}
+
